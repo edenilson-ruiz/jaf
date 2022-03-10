a7ff6bc114dfc54faf473a396aea9d5bae0f735e
José Edenilson Ruiz Ramírez <edenilson.ruiz@ISRI.CENTRAL>
2022-03-09 18:43:04 -0600

    updated welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,30 @@
 @extends('layouts.app')
-@section('title', __('Welcome'))
+@section('title', __('Bienvenido'))
 @section('content')
-<div class="container-fluid">
-<div class="row justify-content-center">
-    <div class="col-md-12">
-        <div class="card">
-            <div class="card-header"><h5><span class="text-center fa fa-home"></span> @yield('title')</h5></div>
-            <div class="card-body">
-              <h5>  
-            @guest
-				
-				{{ __('Welcome to') }} {{ config('app.name', 'Laravel') }} !!! </br>
-				Please contact admin to get your Login Credentials or click "Login" to go to your Dashboard.
-                
-			@else
-					Hi {{ Auth::user()->name }}, Welcome back to {{ config('app.name', 'Laravel') }}.
-            @endif	
-				</h5>
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h5><span class="text-center fa fa-home"></span> @yield('title')</h5>
+                    </div>
+                    <div class="card-body">
+                        @guest
+                            <h5>
+                                {{ __('Bienvenido a') }} {{ config('app.name', 'Laravel') }} !!!
+                            </h5>
+                        @else
+                            <h5>
+                                Hola {{ Auth::user()->name }}, Bienvenido de nuevo a {{ config('app.name', 'Laravel') }}.
+                            </h5>
+                            @endif
+
+                            <p>Herramienta para controlar la entrega de ayudas técnicas a usuarios del ISRI</p>
+                            <p>Por favor contacta con el administrador para obtener tus credenciales o presionar Login si ya tienes usuario.</p>
+
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
-</div>
 @endsection
